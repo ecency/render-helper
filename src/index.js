a5edc222fc48f98b8b8b71ac6099ceb57fc95a3c
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2019-04-30 11:32:44 +0300

    corrections

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ eslint-disable import/prefer-default-export
 */
 
 import proxifyImageSrc from './proxify-image-src';
+import markdown2html from './markdown-2-html';
 
-
-export { proxifyImageSrc };
+export {proxifyImageSrc, markdown2html};
