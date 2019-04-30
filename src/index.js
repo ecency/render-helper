5d610926d37c20e443d55243003187b5f52a6de5
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2019-04-30 16:36:40 +0300

    corrections

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import proxifyImageSrc from './proxify-image-src';
-import markdown2html from './markdown-2-html';
+import renderPostBody from './markdown-2-html';
 import catchPostImage from './catch-post-image';
+import postBodySummary from './post-body-summary';
+import proxifyImageSrc from './proxify-image-src';
 
-export {proxifyImageSrc, markdown2html, catchPostImage};
+export {renderPostBody, catchPostImage, postBodySummary, proxifyImageSrc};
