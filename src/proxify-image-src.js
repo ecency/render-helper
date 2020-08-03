2f9637752e3684d2da1af53d392752b64f93b70c
feruz <fmiboy@gmail.com>
2020-08-03 09:23:12 +0300

    fix test

--- src/proxify-image-src.js
+++ src/proxify-image-src.js
@@ -9,7 +9,7 @@ export default (url, width = 0, height = 0, webp = false) => {
     return '';
   }
 
-  if (webp) proxyBase=`${proxyBase}/webp`;
+  if (webp) proxyBase = `${proxyBase}/webp`;
 
   const prefix = `${proxyBase}/${width}x${height}/`;
   const proxied = `${proxyBase}/0x0/`;
@@ -17,6 +17,6 @@ export default (url, width = 0, height = 0, webp = false) => {
   if (url.startsWith(prefix)) return url;
 
   if (url.startsWith(proxied) || url.startsWith(proxyBase)) return url.replace(proxied, prefix);
-  
+
   return `${prefix}${url}`;
 };
