b1a2dcb3295e825e05a1477e9f8c61683e9ad41e
Ildar Timerbaev <ildar.timerbaev@piano.io>
2021-02-21 17:30:07 +0300

    Move index to TS

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import renderPostBody from './markdown-2-html'
+import catchPostImage from './catch-post-image'
+import postBodySummary from './post-body-summary'
+import proxifyImageSrc, { setProxyBase } from './proxify-image-src'
+import { setCacheSize } from './cache'
+
+export {
+  renderPostBody,
+  catchPostImage,
+  postBodySummary,
+  proxifyImageSrc,
+  setProxyBase,
+  setCacheSize
+}
