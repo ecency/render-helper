d322f0afe016c5f0fe78ef86c9dfccab0a9235a9
Ildar Timerbaev <ildar.timerbaev@piano.io>
2021-02-21 17:31:42 +0300

    Move post-body-summary to TS

---
+++ src/post-body-summary.ts
@@ -0,0 +1,51 @@
+import he from 'he'
+import Remarkable from 'remarkable'
+import { makeEntryCacheKey } from './helper'
+import { cacheGet, cacheSet } from './cache'
+
+const md = new Remarkable({html: true, breaks: true, linkify: false})
+
+function postBodySummary(entryBody: string, length: number): string {
+  if (!entryBody) {
+    return ''
+  }
+
+  // Convert markdown to html
+  let text = md.render(entryBody)
+
+  text = text
+    .replace(/(<([^>]+)>)/gi, '') // Remove html tags
+    .replace(/\r?\n|\r/g, ' ') // Remove new lines
+    .replace(/(?:https?|ftp):\/\/[\n\S]+/g, '') // Remove urls
+    .trim()
+    .replace(/ +(?= )/g, '') // Remove all multiple spaces
+
+  if (length) {
+    // Truncate
+    text = text.substring(0, length)
+  }
+
+  text = he.decode(text) // decode html entities
+
+  return text
+}
+
+function getPostBodySummary(obj: any, length: number): any {
+  if (typeof obj === 'string') {
+    return postBodySummary(obj, length)
+  }
+
+  const key = `${makeEntryCacheKey(obj)}-sum-${length}`
+
+  const item = cacheGet(key)
+  if (item) {
+    return item
+  }
+
+  const res = postBodySummary(obj.body, length)
+  cacheSet(key, res)
+
+  return res
+}
+
+export default postBodySummary
