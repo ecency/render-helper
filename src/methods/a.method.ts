91f35c2489b8bf28336d97138ce0d10b568833f7
Ildar Timerbaev <ildar.timerbaev@piano.io>
2021-02-22 15:08:46 +0300

    Move a to methods

---
+++ src/methods/a.method.ts
@@ -0,0 +1,396 @@
+import {
+  COMMUNITY_REGEX,
+  COPIED_POST_REGEX,
+  D_TUBE_REGEX,
+  IMG_REGEX,
+  IPFS_REGEX,
+  MENTION_REGEX,
+  POST_REGEX,
+  SPEAK_REGEX,
+  TWITCH_REGEX,
+  TWITTER_REGEX,
+  VIMEO_REGEX,
+  WHITE_LIST,
+  YOUTUBE_REGEX
+} from '../consts'
+import { getSerializedInnerHTML } from './get-inner-html.method'
+import proxifyImageSrc from '../proxify-image-src'
+import { removeChildNodes } from './remove-child-nodes.method'
+import xmldom from 'xmldom'
+import { noop } from './noop.method'
+
+function setForAppAttributes(el: HTMLElement, tag: string, author: string, permlink: string): void {
+  el.removeAttribute('href')
+  el.setAttribute('data-tag', tag)
+  el.setAttribute('data-author', author)
+  el.setAttribute('data-permlink', permlink)
+}
+
+export function a(el: HTMLElement, forApp: string, webp: string): void {
+  let href = el.getAttribute('href')
+  const className = el.getAttribute('class')
+
+  // Continue if href has no value
+  // Don't touch user and hashtag links
+  if (!href || ['markdown-author-link', 'markdown-tag-link'].indexOf(className) !== -1) {
+    return
+  }
+
+  // Do not allow js hrefs
+  if (href.startsWith('javascript')) {
+    el.removeAttribute('href')
+    return
+  }
+
+  const DOMParser = new xmldom.DOMParser({
+    errorHandler: { warning: noop, error: noop }
+  })
+
+  // if href is an image url and innerHTML same with href then mark it as image
+  // & => &amp; can break equality
+  if (
+    href.match(IMG_REGEX) &&
+    href.trim().replace(/&amp;/g, '&') ===
+    getSerializedInnerHTML(el).trim().replace(/&amp;/g, '&')
+  ) {
+    const attrs = forApp ?
+      ` data-href="${href}"
+        class="markdown-img-link"
+        src="${proxifyImageSrc(href, 0, 0, webp ? 'webp' : 'match')}"
+      ` :
+      `class="markdown-img-link" src="${proxifyImageSrc(href, 0, 0, webp ? 'webp' : 'match')}"`
+    const replaceNode = DOMParser.parseFromString(`<img ${attrs}/>`)
+    el.parentNode.replaceChild(replaceNode, el)
+    return
+  }
+
+  if (
+    href.match(IPFS_REGEX) &&
+    href.trim().replace(/&amp;/g, '&') ===
+    getSerializedInnerHTML(el).trim().replace(/&amp;/g, '&') &&
+    href.indexOf('#') === -1
+  ) {
+    if (forApp) {
+      el.setAttribute('data-href', href)
+      el.removeAttribute('href')
+    }
+
+    el.setAttribute('class', 'markdown-img-link')
+
+    removeChildNodes(el)
+
+    const img = el.ownerDocument.createElement('img')
+    img.setAttribute('src', href)
+    el.appendChild(img)
+
+    return
+  }
+
+  // If a hive post
+  const postMatch = href.match(POST_REGEX)
+  if (postMatch && WHITE_LIST.includes(postMatch[1])) {
+    el.setAttribute('class', 'markdown-post-link')
+
+    const tag = postMatch[2]
+    const author = postMatch[3].replace('@', '')
+    const permlink = postMatch[4]
+    if (el.textContent === href) {
+      el.textContent = `@${author}/${permlink}`
+    }
+    if (forApp) {
+      setForAppAttributes(el, tag, author, permlink)
+    } else {
+      const h = `/${tag}/@${author}/${permlink}`
+      el.setAttribute('href', h)
+    }
+    return
+  }
+
+  // If a hive user with url
+  const mentionMatch = href.match(MENTION_REGEX)
+  if (mentionMatch && WHITE_LIST.includes(mentionMatch[1]) && mentionMatch.length === 3) {
+    el.setAttribute('class', 'markdown-author-link')
+    const author = mentionMatch[2].replace('@', '').toLowerCase()
+    if (el.textContent === href) {
+      el.textContent = `@${author}`
+    }
+    if (forApp) {
+      el.removeAttribute('href')
+
+      el.setAttribute('data-author', author)
+    } else {
+      const h = `/@${author}`
+      el.setAttribute('href', h)
+    }
+    return
+  }
+
+  // If a copied post link
+  const cpostMatch = href.match(COPIED_POST_REGEX)
+  const matchList = ['wallet', 'points', 'communities', 'posts', 'blog', 'comments', 'replies', 'settings']
+  if (
+    (
+      (cpostMatch && WHITE_LIST.includes(cpostMatch[1].substring(1))) ||
+      (cpostMatch && cpostMatch.length === 4 && cpostMatch[1].indexOf('/') !== 0)
+    ) && !matchList.includes(cpostMatch[3])
+  ) {
+    el.setAttribute('class', 'markdown-post-link')
+
+    let tag = 'post'
+    if (!WHITE_LIST.includes(cpostMatch[1].substring(1))) {
+      [, tag] = cpostMatch
+    }
+
+    const author = cpostMatch[2].replace('@', '')
+    const permlink = cpostMatch[3]
+    if (el.textContent === href) {
+      el.textContent = `@${author}/${permlink}`
+    }
+    if (forApp) {
+      setForAppAttributes(el, tag, author, permlink)
+    } else {
+      const h = `/${tag}/@${author}/${permlink}`
+      el.setAttribute('href', h)
+    }
+
+    return
+  }
+
+  // If a custom hive community link
+  const comMatch = href.match(COMMUNITY_REGEX)
+  if (comMatch && WHITE_LIST.includes(comMatch[1])) {
+    el.setAttribute('class', 'markdown-community-link')
+
+    const community = comMatch[2]
+    let filter = comMatch[3]
+    if (filter === 'about' || filter === 'discord') {
+      filter = 'created'
+    }
+    if (el.textContent === href) {
+      el.textContent = `${filter}/${community}`
+    }
+
+    if (forApp) {
+      el.removeAttribute('href')
+
+      el.setAttribute('data-community', community)
+      el.setAttribute('data-filter', filter)
+    } else {
+      const h = `/${filter}/${community}`
+      el.setAttribute('href', h)
+    }
+    return
+  }
+
+  // If a youtube video
+  if (href.match(YOUTUBE_REGEX) && el.textContent.trim() === href) {
+    const e = YOUTUBE_REGEX.exec(href)
+    if (e[1]) {
+      el.setAttribute('class', 'markdown-video-link markdown-video-link-youtube')
+      el.removeAttribute('href')
+
+      const vid = e[1]
+      const thumbnail = proxifyImageSrc(
+        `https://img.youtube.com/vi/${vid.split('?')[0]}/hqdefault.jpg`,
+        0,
+        0,
+        webp ? 'webp' : 'match'
+      )
+      const embedSrc = `https://www.youtube.com/embed/${vid}?autoplay=1`
+
+      el.textContent = ''
+      el.setAttribute('data-embed-src', embedSrc)
+
+      const thumbImg = el.ownerDocument.createElement('img')
+      thumbImg.setAttribute('class', 'no-replace video-thumbnail')
+      thumbImg.setAttribute('src', thumbnail)
+
+      const play = el.ownerDocument.createElement('span')
+      play.setAttribute('class', 'markdown-video-play')
+
+      el.appendChild(thumbImg)
+      el.appendChild(play)
+
+      return
+    }
+  }
+
+  // If vimeo video
+  if (href.match(VIMEO_REGEX) && href === el.textContent) {
+    const e = VIMEO_REGEX.exec(href)
+    if (e[3]) {
+      el.setAttribute('class', 'markdown-video-link markdown-video-link-vimeo')
+      el.removeAttribute('href')
+
+      const embedSrc = `https://player.vimeo.com/video/${e[3]}`
+
+      el.textContent = ''
+
+      const ifr = el.ownerDocument.createElement('iframe')
+      ifr.setAttribute('frameborder', '0')
+      ifr.setAttribute('allowfullscreen', 'true')
+      ifr.setAttribute('src', embedSrc)
+      el.appendChild(ifr)
+
+      return
+    }
+  }
+
+  // If twitch video
+  if (href.match(TWITCH_REGEX) && href === el.textContent) {
+    const e = TWITCH_REGEX.exec(href)
+    if (e[2]) {
+      el.setAttribute('class', 'markdown-video-link markdown-video-link-twitch')
+      el.removeAttribute('href')
+
+      let embedSrc = ''
+
+      if (e[1] === undefined) {
+        embedSrc = `https://player.twitch.tv/?channel=${e[2]}`
+      } else {
+        embedSrc = `https://player.twitch.tv/?video=${e[1]}`
+      }
+
+      el.textContent = ''
+
+      const ifr = el.ownerDocument.createElement('iframe')
+      ifr.setAttribute('frameborder', '0')
+      ifr.setAttribute('allowfullscreen', 'true')
+      ifr.setAttribute('src', embedSrc)
+      el.appendChild(ifr)
+
+      return
+    }
+  }
+
+  // If a d.tube video
+  if (href.match(D_TUBE_REGEX)) {
+    // Only d.tube links contains an image
+    const imgEls = el.getElementsByTagName('img')
+
+    if (imgEls.length === 1) {
+      const e = D_TUBE_REGEX.exec(href)
+      // e[2] = username, e[3] object id
+      if (e[2] && e[3]) {
+        el.setAttribute('class', 'markdown-video-link markdown-video-link-dtube')
+        el.removeAttribute('href')
+
+        const thumbnail = proxifyImageSrc(
+          imgEls[0].getAttribute('src').replace(/\s+/g, ''),
+          0,
+          0,
+          webp ? 'webp' : 'match'
+        )
+        const videoHref = `https://emb.d.tube/#!/${e[2]}/${e[3]}`
+
+        // el.setAttribute('data-video-href', videoHref);
+        el.setAttribute('data-embed-src', videoHref)
+
+        const thumbImg = el.ownerDocument.createElement('img')
+        thumbImg.setAttribute('class', 'no-replace video-thumbnail')
+        thumbImg.setAttribute('itemprop', 'image')
+        thumbImg.setAttribute('src', thumbnail)
+
+        const play = el.ownerDocument.createElement('span')
+        play.setAttribute('class', 'markdown-video-play')
+
+        el.appendChild(thumbImg)
+        el.appendChild(play)
+
+        // Remove image.
+        el.removeChild(imgEls[0])
+
+        return
+      }
+    }
+  }
+
+  // Detect 3Speak
+  if (href.match(SPEAK_REGEX)) {
+    const imgEls = el.getElementsByTagName('img')
+
+    if (imgEls.length === 1) {
+      const e = SPEAK_REGEX.exec(href)
+      // e[1] = tld , e[3] = embed address
+      if (e[1] && e[3]) {
+        el.setAttribute('class', 'markdown-video-link markdown-video-link-speak')
+        el.removeAttribute('href')
+
+        const thumbnail = proxifyImageSrc(
+          imgEls[0].getAttribute('src').replace(/\s+/g, ''),
+          0,
+          0,
+          webp ? 'webp' : 'match'
+        )
+        const videoHref = `https://3speak.${e[1]}/embed?v=${e[3]}`
+
+        // el.setAttribute('data-video-href', videoHref);
+        el.setAttribute('data-embed-src', videoHref)
+
+        const thumbImg = el.ownerDocument.createElement('img')
+        thumbImg.setAttribute('class', 'no-replace video-thumbnail')
+        thumbImg.setAttribute('itemprop', 'image')
+        thumbImg.setAttribute('src', thumbnail)
+
+        const play = el.ownerDocument.createElement('span')
+        play.setAttribute('class', 'markdown-video-play')
+
+        el.appendChild(thumbImg)
+        el.appendChild(play)
+
+        // Remove image.
+        el.removeChild(imgEls[0])
+
+        return
+      }
+    }
+  }
+
+  // If tweets
+  if (href.match(TWITTER_REGEX) && el.textContent.trim() === href) {
+    const e = TWITTER_REGEX.exec(href)
+    if (e) {
+      const url = e[0].replace(/(<([^>]+)>)/gi, '')
+      const author = e[1].replace(/(<([^>]+)>)/gi, '')
+
+      const twitterCode = `<blockquote class="twitter-tweet"><p>${url}</p>- <a href="${url}">${author}</a></blockquote>`
+      const replaceNode = DOMParser.parseFromString(twitterCode)
+      el.parentNode.replaceChild(replaceNode, el)
+      return
+    }
+  }
+
+  if (href.indexOf('https://hivesigner.com/sign/account-witness-vote?witness=') === 0 && forApp) {
+    el.setAttribute('class', 'markdown-witnesses-link')
+    el.setAttribute('data-href', href)
+    el.removeAttribute('href')
+    return
+  }
+
+  if (href.indexOf('hivesigner.com/sign/update-proposal-votes?proposal_ids') > 0 && forApp) {
+    const m = decodeURI(href).match(/proposal_ids=\[(\d+)]/)
+    if (m) {
+      el.setAttribute('class', 'markdown-proposal-link')
+      el.setAttribute('data-href', href)
+      el.setAttribute('data-proposal', m[1])
+      el.removeAttribute('href')
+      return
+    }
+  }
+
+  // If nothing matched element as external link so it will be opened in external window
+  el.setAttribute('class', 'markdown-external-link')
+
+  // Prepend https if no scheme provided
+  if (!(/^((#)|(mailto:)|(\/(?!\/))|(((steem|hive|esteem|ecency|https?):)?\/\/))/.test(href))) {
+    href = `https://${href}`
+  }
+
+  if (forApp) {
+    el.setAttribute('data-href', href)
+    el.removeAttribute('href')
+  } else {
+    el.setAttribute('target', '_blank')
+    el.setAttribute('rel', 'noopener')
+  }
+}
