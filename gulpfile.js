dca63542cb85a95f3ea0f7c6e14084ab275544e4
demogoran <demogoran@gmail.com>
2021-06-14 17:31:18 +0300

    Updated compression && install missed dependency

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,28 @@
-var gulp = require('gulp');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var tsify = require('tsify');
-var sourcemaps = require('gulp-sourcemaps');
-var buffer = require('vinyl-buffer');
-var uglify = require('gulp-uglify');
-const pkg = require('./package.json');
+var gulp = require("gulp");
+var browserify = require("browserify");
+var source = require("vinyl-source-stream");
+var tsify = require("tsify");
+var sourcemaps = require("gulp-sourcemaps");
+var buffer = require("vinyl-buffer");
+var uglify = require("gulp-uglify");
+const pkg = require("./package.json");
 
 const libraryName = pkg.name;
 
 function build() {
   return browserify({
-    basedir: '.',
-    entries: ['src/index.ts'],
+    basedir: ".",
+    entries: ["src/index.ts"],
     cache: {},
     standalone: libraryName,
-    packageCache: {}
+    packageCache: {},
   })
-    .plugin(tsify, {target: 'es5', module: 'commonjs'})
+    .plugin(tsify, { target: "es5", module: "commonjs" })
     .bundle()
-    .pipe(source('render-helper.js'))
-    .pipe(gulp.dest('lib'));
+    .pipe(source("render-helper.js"))
+    .pipe(buffer()) //you cannot get rid of this.
+    .pipe(uglify())
+    .pipe(gulp.dest("lib"));
 }
 
 exports.default = gulp.series(build);
