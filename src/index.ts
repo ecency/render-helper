b07755ddb1abba13bda2dc3481fb53345186c3db
demogoran <demogoran@gmail.com>
2021-06-24 09:32:23 +0300

    Rename to renderAmpBody

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { markdown2Html as renderPostBody } from "./markdown-2-html";
-import { markdown2AMP as renderPostAMP } from "./markdown-2-amp";
+import { markdown2AMP as renderAmpBody } from "./markdown-2-amp";
 import { catchPostImage } from "./catch-post-image";
 import { getPostBodySummary as postBodySummary } from "./post-body-summary";
 import { setProxyBase, proxifyImageSrc } from "./proxify-image-src";
@@ -7,7 +7,7 @@ import { setCacheSize } from "./cache";
 
 export {
   renderPostBody,
-  renderPostAMP,
+  renderAmpBody,
   catchPostImage,
   postBodySummary,
   proxifyImageSrc,
