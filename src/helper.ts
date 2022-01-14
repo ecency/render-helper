e91d69a25ad996e4e9ed0693a0e15525fafe2d87
noumantahir <nouman.hawkdev@gmail.com>
2022-01-14 19:14:53 +0500

    helper method to extract start time

--- src/helper.ts
+++ src/helper.ts
@@ -14,3 +14,14 @@ export function makeEntryCacheKey(entry: any): string {
   return `${entry.author}-${entry.permlink}-${entry.last_update}`
 }
 
+export function extractYtStartTime(url:string):string {
+  const urlObj = new URL(url);
+  const params = new URLSearchParams(urlObj.search);
+  if(params.has('t')){
+    return '' + parseInt(params.get('t')); //parsing is important as sometimes t is famated '123s';
+  }else if (params.has('start')){
+    return params.get('start');
+  }
+
+  return '';
+}
