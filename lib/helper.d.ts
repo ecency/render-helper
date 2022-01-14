bfb44f0816bb2aeb5eb8dd4032af7f744a07631a
feruz <fmiboy@gmail.com>
2022-01-14 21:44:04 +0200

    version bump

--- lib/helper.d.ts
+++ lib/helper.d.ts
@@ -1,2 +1,3 @@
 export declare function createDoc(html: string): Document | null;
 export declare function makeEntryCacheKey(entry: any): string;
+export declare function extractYtStartTime(url: string): string;
