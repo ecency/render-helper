c1134d029e344be65a1ebbd99e698470a8fbda00
feruz <fmiboy@gmail.com>
2022-06-13 09:43:08 +0300

    fix youtube time catch

--- src/helper.ts
+++ src/helper.ts
@@ -15,13 +15,15 @@ export function makeEntryCacheKey(entry: any): string {
 }
 
 export function extractYtStartTime(url:string):string {
-  const urlObj = new URL(url);
-  const params = new URLSearchParams(urlObj.search);
-  if(params.has('t')){
-    return '' + parseInt(params.get('t')); //parsing is important as sometimes t is famated '123s';
-  }else if (params.has('start')){
-    return params.get('start');
+  try {
+    const urlObj = new URL(url);
+    const params = new URLSearchParams(urlObj.search);
+    if(params.has('t')){
+      return '' + parseInt(params.get('t')); //parsing is important as sometimes t is famated '123s';
+    }else if (params.has('start')){
+      return params.get('start');
+    } 
+  } catch (error) {
+    return '';
   }
-
-  return '';
 }
