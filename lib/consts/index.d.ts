d34cc05577dd081715f7003eda3591c05d1802cd
feruz <fmiboy@gmail.com>
2022-06-13 09:49:25 +0300

    update build

--- lib/consts/index.d.ts
+++ lib/consts/index.d.ts
@@ -1,4 +1,5 @@
 export * from './white-list.const';
+export * from './section-list.const';
 export * from './regexes.const';
 export * from './allowed-attributes.const';
 export * from './dom-parser.const';
---
+++ lib/consts/section-list.const.d.ts
@@ -0,0 +1,1 @@
+export declare const SECTION_LIST: string[];
